1614d5e95fd2eb0103460fa3115c7f47182ac283
Dacian22 <balintdacian@yahoo.com>
2023-10-12 01:18:00 +0300

    Added second part of the third problem

--- src/BigNumbers.java
+++ src/BigNumbers.java
@@ -11,6 +11,29 @@ public class BigNumbers {
             result.add(digit);
         }
 
+        if (overflow ==1){
+            result.add(overflow);
+        }
+
+        return result;
+    }
+
+    public ArrayList<Integer> difference( int[] number1, int[] number2){
+        int overflow = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int i = number1.length - 1; i>=0;i--){
+            int digit = (number1[i]-number2[i]-overflow)%10;
+            if(digit < 0){
+                digit +=10;
+            }
+            overflow = (number1[i]-number2[i]-overflow)/10 * -1;
+            result.add(digit);
+        }
+
+        if (overflow ==1){
+            result.add(overflow);
+        }
+
         return result;
     }
 }
