4b44fb7a2b0fcfc6bf0a50708c0fe6bbcea7408a
Dacian22 <balintdacian@yahoo.com>
2023-10-12 01:08:38 +0300

    Added last part of the second problem

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -32,4 +32,15 @@ public class ArrayOperations {
         return sum - min;
     }
 
+    int minimal_sum(int[] array){
+        int sum = 0;
+        int max= this.maximum(array);
+
+        for (int i = 0 ;i< array.length;i++){
+            sum += array[i];
+        }
+
+        return sum - max;
+    }
+
 }
