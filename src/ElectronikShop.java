332bc5a6f71ce27997c250ae704b0a517651d82f
Dacian22 <balintdacian@yahoo.com>
2023-10-13 17:10:41 +0300

    Last problem

---
+++ src/ElectronikShop.java
@@ -0,0 +1,49 @@
+public class ElectronikShop {
+    public int billigsteTastatur(int[] tastaturen){
+        int min = Integer.MAX_VALUE;
+        for (int i = 0 ;i< tastaturen.length;i++){
+            if(tastaturen[i] < min){
+                min = tastaturen[i];
+            }
+        }
+
+        return min;
+    }
+
+    public int teurersteGegenstand(int[] gegenstand){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0 ;i< gegenstand.length;i++){
+            if(gegenstand[i] > max){
+                max = gegenstand[i];
+            }
+        }
+
+        return max;
+    }
+
+    public int teuersteLaufwerk(int[] price, int budget){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0 ;i< price.length;i++){
+            if(price[i] > max && price[i] < budget){
+                max = price[i];
+            }
+        }
+
+        return max;
+    }
+
+
+    public int geldbetrag(int budget, int[] tastaturen, int[] laufwerk){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0 ;i< tastaturen.length;i++){
+            for (int j = 0 ;j< laufwerk.length;j++) {
+                if (laufwerk[j] +tastaturen[i]  > max && (laufwerk[j] +tastaturen[i]) < budget) {
+                    max = laufwerk[j] +tastaturen[i];
+                }
+            }
+        }
+
+        return max;
+    }
+
+}
